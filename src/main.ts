3014b0a99b4523bf77a9f19cd214c108e6585a60
ZS iMac <zsolis@deltacontrols.com>
2024-11-11 13:12:54 -0800

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API sandbox')
+    .setDescription('This is a sandbox to play with API endpoints')
+    .setVersion('1.0')
+    .addTag('sandbox')
+    .addTag('testing')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
